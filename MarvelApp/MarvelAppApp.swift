74581c7a037b4ef68abe30d735be2f90af6ac4fc
Kevin Heredia <kevin@ravecodesolutions.com>
2024-12-09 18:12:48 -0500

    Git init

---
+++ MarvelApp/MarvelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppApp.swift
+//  MarvelApp
+//
+//  Created by Kevin Heredia on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
